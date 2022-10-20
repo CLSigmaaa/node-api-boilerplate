9faa270208beedb4b42be34b366895d7d6a89bb6
OniX <gauthier.delzenne@gmail.com>
2022-10-20 15:34:08 +0200

    Middleware update

--- src/middlewares/blacklist.js
+++ src/middlewares/blacklist.js
@@ -1,9 +1,7 @@
-module.exports = (app) => {
-    app.use((req, res, next) => {
-        let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        if(ip === '::ffff:172.17.2.96' || ip === "::ffff:172.17.2.204"){
-            const message = "T'es banni fdp";
-            res.status(403).json({message});
-        }
-    })
+module.exports = (req, res, next) => {
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    if(ip === '::ffff:172.17.2.96' || ip === "::ffff:172.17.2.204"){
+        const message = "T'es banni fdp";
+        res.status(403).json({message});
+    }
 }
